fdfc6a252f594781b3440083d37980e8063983fa
Yakeing <yakeing@gmail.com>
2019-09-01 23:33:35 +0800

    Update php_badgeTest.php

--- tests/php_badgeTest.php
+++ tests/php_badgeTest.php
@@ -5,12 +5,18 @@ use php_badge\Badge;
 use PHPUnit\Framework\TestCase;
 class php_badgeTest extends TestCase{
   public function testBadge(){
+    $str = 'php_badge';
     $path = dirname(__FILE__);
     $FontFile = $path.'/DejaVu-Sans.ttf'; //font file path
     $DBsvg = $path.'/DB.svg';
     $CIsvg = $path.'/CI.svg';
     $Badge = new Badge();
     $Badge->imageFontFile = $FontFile;
+    //----------- STR --------------//
+    ob_start();
+      $Badge->svg($str);
+    $ob_STR = ob_get_contents();
+    ob_end_clean();
     //----------- DB --------------//
     $DB = array(
       array('state', '44CC11'),
@@ -21,7 +27,6 @@ class php_badgeTest extends TestCase{
       $Badge->svg($DB);
     $ob_DB = ob_get_contents();
     ob_end_clean();
-    //$this->assertXmlStringEqualsXmlFile($DBsvg, $ob_DB);
     //----------- CI --------------//
      $CI = array(
         array('build', '555'), //#555555
@@ -34,8 +39,12 @@ class php_badgeTest extends TestCase{
     ob_end_clean();
     file_put_contents('/tmp/DB.svg', $ob_DB);
     file_put_contents('/tmp/CI.svg', $ob_CI);
-    //$this->assertXmlStringEqualsXmlFile($CIsvg, $ob_CI);
+    file_put_contents('/tmp/STR.svg', $ob_STR);
     $this->assertFileExists('/tmp/DB.svg');
     $this->assertFileExists('/tmp/CI.svg');
+    $this->assertFileExists('/tmp/STR.svg');
+    //$this->assertXmlStringEqualsXmlFile('/tmp/STR.svg', $str);
+    $this->assertXmlFileEqualsXmlFile($DBsvg, '/tmp/DB.svg');
+    $this->assertXmlFileEqualsXmlFile($CIsvg, '/tmp/CI.svg');
   }
 }
