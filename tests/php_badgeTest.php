04d4b1b4aa661fc01e42c723b0e4fd7fe2129e96
Yakeing <yakeing@gmail.com>
2019-09-15 14:30:56 +0800

    Update php_badgeTest.php

--- tests/php_badgeTest.php
+++ tests/php_badgeTest.php
@@ -5,46 +5,37 @@ use php_badge\Badge;
 use PHPUnit\Framework\TestCase;
 class php_badgeTest extends TestCase{
   public function testBadge(){
-    $str = 'Test';
     $path = dirname(__FILE__);
-    $FontFile = $path.'/DejaVu-Sans.ttf'; //font file path
-    $DBsvg = $path.'/DB.svg';
-    $CIsvg = $path.'/CI.svg';
     $Badge = new Badge();
-    $Badge->imageFontFile = $FontFile;
+    $Badge->imageFontFile = $path.'/DejaVu-Sans.ttf'; //font file path
     //----------- STR --------------//
+    $str = 'Test';
     ob_start();
       $Badge->svg($str);
     $ob_STR = ob_get_contents();
     ob_end_clean();
-    //----------- DB --------------//
-    $DB = array(
-      array('state', '44CC11'),
-      array('staging', 'F0CE1A'),
-      array('testing', 'A0ABFC')
+    //file_put_contents('/tmp/STR.svg', $ob_STR);
+    //$this->assertFileExists('/tmp/STR.svg');
+    //$this->assertXmlStringEqualsXmlFile('/tmp/STR.svg', $str);
+    $Badge->OutputNo = false; //Return string
+    //----------- Splicing --------------//
+    $db_Splicing = array(
+        array('hits','555555'),
+        array('12345','4C1')
     );
-    ob_start();
-      $Badge->svg($DB);
-    $ob_DB = ob_get_contents();
-    ob_end_clean();
-    //----------- CI --------------//
-     $CI = array(
+    $ob_Splicing = $Badge->svg($db_Splicing);
+    file_put_contents('/tmp/Splicing.svg', $ob_Splicing);
+    $this->assertFileExists('/tmp/Splicing.svg');
+    //$this->assertXmlFileEqualsXmlFile($path.'/Splicing.svg', '/tmp/Splicing.svg');
+    //----------- Simplexml --------------//
+    $Badge->SimplexmlNo = true; //Simplexml Svg
+     $db_Simplexml = array(
         array('build', '555'), //#555555
         array('passing', '4C1'), //#44CC11
     );
-    $Badge->SimplexmlNo = true; //Simplexml Svg
-    ob_start();
-      $Badge->svg($CI);
-    $ob_CI = ob_get_contents();
-    ob_end_clean();
-    file_put_contents('/tmp/DB.svg', $ob_DB);
-    file_put_contents('/tmp/CI.svg', $ob_CI);
-    file_put_contents('/tmp/STR.svg', $ob_STR);
-    $this->assertFileExists('/tmp/DB.svg');
-    $this->assertFileExists('/tmp/CI.svg');
-    $this->assertFileExists('/tmp/STR.svg');
-    //$this->assertXmlStringEqualsXmlFile('/tmp/STR.svg', $str);
-    //$this->assertXmlFileEqualsXmlFile($DBsvg, '/tmp/DB.svg');
-    //$this->assertXmlFileEqualsXmlFile($CIsvg, '/tmp/CI.svg');
+    $ob_Simplexml = $Badge->svg($db_Simplexml);
+    file_put_contents('/tmp/Simplexml.svg', $ob_Simplexml);
+    $this->assertFileExists('/tmp/Simplexml.svg');
+    $this->assertXmlFileEqualsXmlFile($path.'/Simplexml.svg', '/tmp/Simplexml.svg');
   }
 }
